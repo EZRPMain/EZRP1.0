8fbdaa45c7558bc0e83a733afd77a0290b2717c9
Jay <80186604+jay-fivem@users.noreply.github.com>
2023-07-26 16:38:49 +0930

    fix small err

--- resources/cayo_missions/client/cl_spacestrike.lua
+++ resources/cayo_missions/client/cl_spacestrike.lua
@@ -32,8 +32,8 @@ end
 RegisterNetEvent("client:spacestrike", function()
     local color = {r = 255, g = 255, b = 255, a = 255}
     local alreadyOpened = false
-    local text = ' [E] Space Strike [←] Cancel'
-    exports['qb-core']:Notify(text)
+    local text = '[E] Space Strike [←] Cancel'
+    -- exports['qb-core']:Notify(text)
     ExecuteCommand("e radio")
     while true do
         local hit, dest, _, _ = RayCastCamera(1000)
@@ -54,26 +54,24 @@ RegisterNetEvent("client:spacestrike", function()
                 exports['qb-core']:HideText()
                 for i=10,1,-1 do
                     Wait(1000)
-                    QBCore.Functions.Notify(("Space strike in %s seconds"):format(i),nil,990)
+                    exports['qb-core']:Notify(("Space strike in %s seconds"):format(i),nil,990)
                 end
                 RequestWeaponAsset(`WEAPON_AIRSTRIKE_ROCKET`) 
-                print("test")
                 while not HasWeaponAssetLoaded(`WEAPON_AIRSTRIKE_ROCKET`) do
                     Wait(0)
-                    print("test2")
                 end
                 ShootSingleBulletBetweenCoords(dest.x + math.random(-5,5), dest.y + math.random(-5,5), dest.z+150, dest.x, dest.y, dest.z, 500, true,`WEAPON_AIRSTRIKE_ROCKET`, PlayerPedId(), true, false, -1.0)
 
-                for i=1,19 do
+                for i=1,math.random(15,30) do
                     Wait(math.random(300,1300))
                     ShootSingleBulletBetweenCoords(dest.x + math.random(-5,5), dest.y + math.random(-5,5), dest.z+150, dest.x+ math.random(-15,15), dest.y+ math.random(-15,15), dest.z, 500, true,`WEAPON_AIRSTRIKE_ROCKET`, PlayerPedId(), true, false, -1.0)
                 end
-                print("test3")
                 CancelEvent()
                 break
             elseif IsControlJustReleased(0, 202) then
                 ExecuteCommand("e c")
-                TriggerEvent('cd_drawtextui:HideUI')
+                -- TriggerEvent('cd_drawtextui:HideUI')
+                exports['qb-core']:HideText()
                 CancelEvent()
                 break
             end
@@ -94,7 +92,7 @@ end)
 --             if IsControlJustReleased(0, 38) then
 --                 for i=10,1,-1 do
 --                     Wait(1000)
---                     QBCore.Functions.Notify(("Space strike in %s seconds"):format(i),nil,990)
+--                     exports['qb-core']:Notify(("Space strike in %s seconds"):format(i),nil,990)
 --                 end
 --                 RequestWeaponAsset(`VEHICLE_WEAPON_SPACE_ROCKET`) 
 --                 while not HasWeaponAssetLoaded(`VEHICLE_WEAPON_SPACE_ROCKET`) do
