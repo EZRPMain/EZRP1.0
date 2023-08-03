aceda56ca5f3cd90451be668cf764dd07c9c5872
Jay <80186604+jay-fivem@users.noreply.github.com>
2023-08-03 19:50:09 +0930

    can only sell exotic fish at night

--- resources/[resources-custom]/ez-fishing/client/addons.lua
+++ resources/[resources-custom]/ez-fishing/client/addons.lua
@@ -362,62 +362,61 @@ end)
 
 RegisterNetEvent('ez-fishing:client:SellillegalFish')
 AddEventHandler('ez-fishing:client:SellillegalFish', function() 
-	-- QBCore.Functions.TriggerCallback('QBCore:HasItem', function(HasItem)
     local HasItem = QBCore.Functions.HasItem("pearlscard")
-		if HasItem then
-			local charinfo = QBCore.Functions.GetPlayerData().charinfo
-			QBCore.Functions.Notify('Welcome, '..charinfo.firstname..' '..charinfo.lastname)
-			exports['qb-menu']:openMenu({
-				{
-					header = "Pearl's Seafood Restaurant",
-					isMenuHeader = true
-				},
-				{
-					header = "Sell Dolphin",
-					txt = "Current Price: $"..Config.dolphinPrice.." each",
-					params = {
-						isServer = true,
-						event = "fishing:server:SellillegalFish",
-						args = 1
-					}
-				},
-				{
-					header = "Sell Tiger Shark",
-					txt = "Current Price: $"..Config.sharktigerPrice.." each",
-					params = {
-						isServer = true,
-						event = "fishing:server:SellillegalFish",
-						args = 2
-					}
-				},
-				{
-					header = "Sell Hammerhead Shark",
-					txt = "Current Price: $"..Config.sharkhammerPrice.." each",
-					params = {
-						isServer = true,
-						event = "fishing:server:SellillegalFish",
-						args = 3
-					}
-				},
-				{
-					header = "Sell Orca",
-					txt = "Current Price: $"..Config.killerwhalePrice.." each",
-					params = {
-						isServer = true,
-						event = "fishing:server:SellillegalFish",
-						args = 4
-					}
-				},
-				{
-					header = "Close",
-					txt = "",
-					params = {
-						event = "qb-menu:closeMenu"
-					}
-				},
-			})
-		else
-			QBCore.Functions.Notify('You cannot sell to us at the moment, sorry for the inconvenience', 'error', 3500)
-		end
-	-- end, "pearlscard")
+    if HasItem and (GetClockHours() < 6 or GetClockHours() > 22) then
+        local charinfo = QBCore.Functions.GetPlayerData().charinfo
+        QBCore.Functions.Notify('Welcome, '..charinfo.firstname..' '..charinfo.lastname)
+        exports['qb-menu']:openMenu({
+            {
+                header = "Pearl's Seafood Restaurant",
+                isMenuHeader = true
+            },
+            {
+                header = "Sell Dolphin",
+                txt = "Current Price: $"..Config.dolphinPrice.." each",
+                params = {
+                    isServer = true,
+                    event = "fishing:server:SellillegalFish",
+                    args = 1
+                }
+            },
+            {
+                header = "Sell Tiger Shark",
+                txt = "Current Price: $"..Config.sharktigerPrice.." each",
+                params = {
+                    isServer = true,
+                    event = "fishing:server:SellillegalFish",
+                    args = 2
+                }
+            },
+            {
+                header = "Sell Hammerhead Shark",
+                txt = "Current Price: $"..Config.sharkhammerPrice.." each",
+                params = {
+                    isServer = true,
+                    event = "fishing:server:SellillegalFish",
+                    args = 3
+                }
+            },
+            {
+                header = "Sell Orca",
+                txt = "Current Price: $"..Config.killerwhalePrice.." each",
+                params = {
+                    isServer = true,
+                    event = "fishing:server:SellillegalFish",
+                    args = 4
+                }
+            },
+            {
+                header = "Close",
+                txt = "",
+                params = {
+                    event = "qb-menu:closeMenu"
+                }
+            },
+        })
+    else
+        QBCore.Functions.Notify('You cannot sell to us at the moment, sorry for the inconvenience', 'error', 3500)
+    end
+
 end)
