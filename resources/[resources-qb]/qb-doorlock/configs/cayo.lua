1e23446b4bbef56973c64403af7c7cbf222e43cc
Jay <80186604+jay-fivem@users.noreply.github.com>
2023-08-10 15:21:32 +0930

    cayomansion retextture removeal

---
+++ resources/[resources-qb]/qb-doorlock/configs/cayo.lua
@@ -0,0 +1,15 @@
+
+
+-- cayogate1 created by Jay
+Config.DoorList['cayo-cayogate1'] = {
+    doorLabel = 'cayogate1',
+    doorRate = 1.0,
+    authorizedJobs = { ['unemployed'] = 0 },
+    objYaw = 72.63680267334,
+    fixText = false,
+    objCoords = vec3(4972.143066, -5764.132324, 21.083078),
+    locked = false,
+    distance = 2,
+    objName = -1697935936,
+    doorType = 'door',
+}
