d9152e5d34441e81802c724a623d93adee3bdf5b
Jay <80186604+jay-fivem@users.noreply.github.com>
2023-08-10 20:46:25 +0930

    map fixes

---
+++ resources/[resources-qb]/qb-doorlock/configs/test.lua
@@ -0,0 +1,15 @@
+
+
+-- test created by Jay
+Config.DoorList['test-test'] = {
+    doors = {
+        {objName = 62988052, objYaw = 89.999992370605, objCoords = vec3(392.264618, -751.229004, 28.682714)},
+        {objName = 62988052, objYaw = 270.0, objCoords = vec3(392.273438, -753.423462, 28.682714)}
+    },
+    doorRate = 1.0,
+    doorLabel = 'test',
+    authorizedJobs = { ['unemployed'] = 0 },
+    locked = false,
+    doorType = 'doublesliding',
+    distance = 5,
+}
