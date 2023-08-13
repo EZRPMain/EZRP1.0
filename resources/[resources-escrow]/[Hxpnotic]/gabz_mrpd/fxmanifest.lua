accdb1f42e1a27269b431b72e0e8931c5bf3c6da
Hxpnotic1 <142072080+Hxpnotic1@users.noreply.github.com>
2023-08-13 17:07:11 -0500

    no mrpd

--- resources/[resources-escrow]/[Hxpnotic]/gabz_mrpd/fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-this_is_a_map 'yes'
-files {
-	'common/*.meta'
-}
-data_file 'DLC_ITYP_REQUEST' 'stream/gabz_mrpd_ext_props.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/gabz_mrpd_int.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/gabz_mrpd_props.ytyp'
