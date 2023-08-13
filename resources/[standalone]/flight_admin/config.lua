02379547b67f491b6fa346c0f648289581e47059
Hxpnotic1 <142072080+Hxpnotic1@users.noreply.github.com>
2023-08-13 14:23:28 -0500

    Admin

---
+++ resources/[standalone]/flight_admin/config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.language = 'en' -- Available Languages : en/de
+
+Config.openMenuKey = 'U'
+Config.toggleNoclipKey = 'F2'
+Config.teleportMarkerKey = 'F10'
+
+Config.perimission =  function(type)
+    return true
+end
