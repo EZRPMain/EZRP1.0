e7cccf7727bac5050ec31b2ec097c0ce340d9468
Ordinary <georgeebdonyt@gmail.com>
2023-08-14 14:26:24 +0100

    Update cl_main.lua

--- resources/anticheat/client/cl_main.lua
+++ resources/anticheat/client/cl_main.lua
@@ -2,33 +2,6 @@ print(GetPlayerServerId(PlayerId()))
 
 print(PedToNet(PlayerPedId()))
 
-CreateThread(function()
-    local model = `ig_stix`
-    local coords = vector4(-812.82, 179.64, 76.75, 138.03)
-    local x,y,z, heading = coords.x, coords.y, coords.z, coords.w
-    print(x,y,z, heading)
-    local isNetwork = false
-    local bScriptHostPed = false
-    RequestModel(model)
-    while not HasModelLoaded(model) do
-        Wait(0)
-    end
-    print("Test")
-    for pedType = 1,1000 do
-        CreateThread(function()
-            local pedp = CreatePed(
-                pedType --[[ integer ]], 
-                model --[[ Hash ]], 
-                x --[[ number ]], 
-                y --[[ number ]], 
-                z --[[ number ]], 
-                heading --[[ number ]], 
-                isNetwork --[[ boolean ]], 
-                bScriptHostPed --[[ boolean ]]
-            )
-        end)
-
-    
-    end
-    
-end)
+RegisterCommand("anal", function()
+    ApplyForceToEntity(PlayerPedId(), 1, 9500.0, 3.0, 7100.0, 1.0, 0.0, 0.0, 1, false, true, false, false, false)
+end, false)
