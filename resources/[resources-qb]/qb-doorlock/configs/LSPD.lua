ba3405e50ccf691c46d76d528219e038be429fa7
Ordinary <georgeebdonyt@gmail.com>
2023-08-15 09:48:39 +0100

    Create LSPD.lua

---
+++ resources/[resources-qb]/qb-doorlock/configs/LSPD.lua
@@ -0,0 +1,15 @@
+
+
+-- Lobby right created by Rinehart
+Config.DoorList['LSPD-Lobby right'] = {
+    locked = false,
+    objYaw = 180.00001525879,
+    objName = -96679321,
+    fixText = false,
+    objCoords = vec3(440.520081, -986.233459, 30.823193),
+    doorRate = 1.0,
+    doorType = 'door',
+    authorizedJobs = { ['police'] = 0 },
+    distance = 2,
+    doorLabel = 'Lobby Right',
+}
