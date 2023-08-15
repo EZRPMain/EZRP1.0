e11acd542575860f2f475451d61220768662c6d2
Jay <80186604+jay-fivem@users.noreply.github.com>
2023-08-15 21:19:18 +0930

    scuff fix for death

--- resources/ez-scuffs/client/cl_main.lua
+++ resources/ez-scuffs/client/cl_main.lua
@@ -1,5 +1,9 @@
 RegisterNetEvent("scuff-fix", function()
-    SetEntityCoords(PlayerPedId(), GetEntityCoords(PlayerPedId()))
+    local ped = PlayerPedId()
+    local coords = GetEntityCoords(ped)
+    if IsEntityDead(ped) then
+        SetEntityCoords(ped, coords)
+    end
 end)
 
 RegisterCommand("scuff-fix", function()
