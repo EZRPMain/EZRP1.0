d03d537ad3d3b0a8599b76f973da19aae01d2ea6
Ordinary <georgeebdonyt@gmail.com>
2023-08-15 11:35:17 +0100

    Create Aztecas.lua

---
+++ resources/[resources-qb]/qb-doorlock/configs/Aztecas.lua
@@ -0,0 +1,168 @@
+
+
+-- Gate created by Rinehart
+Config.DoorList['Aztecas-Gate'] = {
+    authorizedGangs = { ['aztecas'] = 0 },
+    doors = {
+        {objName = -1646581307, objYaw = 174.00001525879, objCoords = vec3(985.573425, -2480.550537, 27.276766)},
+        {objName = -1646581307, objYaw = 354.00619506836, objCoords = vec3(996.604919, -2481.709961, 27.276766)}
+    },
+    distance = 7,
+    locked = true,
+    doorLabel = 'Gate',
+    doorRate = 1.0,
+    doorType = 'doublesliding',
+}
+
+-- Garage 1 created by Rinehart
+Config.DoorList['Aztecas-Garage 1'] = {
+    objYaw = 265.00006103516,
+    fixText = false,
+    doorRate = 1.0,
+    doorLabel = 'garage 1',
+    authorizedGangs = { ['aztecas'] = 0 },
+    distance = 2,
+    locked = true,
+    objName = 470340995,
+    objCoords = vec3(1019.153320, -2516.939209, 28.944899),
+    doorType = 'garage',
+}
+
+-- Garage 2 created by Rinehart
+Config.DoorList['Aztecas-Garage 2'] = {
+    objYaw = 264.79998779297,
+    fixText = false,
+    doorRate = 1.0,
+    doorLabel = 'garage 2',
+    authorizedGangs = { ['aztecas'] = 0 },
+    distance = 2,
+    locked = true,
+    objName = 470340995,
+    objCoords = vec3(1018.772278, -2521.640137, 28.946398),
+    doorType = 'garage',
+}
+
+-- Garage 3 created by Rinehart
+Config.DoorList['Aztecas-Garage 3'] = {
+    objYaw = 264.69998168945,
+    fixText = false,
+    doorRate = 1.0,
+    doorLabel = 'garage 3',
+    authorizedGangs = { ['aztecas'] = 0 },
+    distance = 2,
+    locked = true,
+    objName = 470340995,
+    objCoords = vec3(1018.343872, -2526.468750, 28.949532),
+    doorType = 'garage',
+}
+
+--  Side 1 created by Rinehart
+Config.DoorList['Aztecas- Side 1'] = {
+    objCoords = vec3(1019.674011, -2511.043457, 28.518326),
+    locked = true,
+    objYaw = 84.999969482422,
+    distance = 2,
+    doorRate = 1.0,
+    fixText = false,
+    objName = -698797376,
+    doorType = 'door',
+    authorizedGangs = { ['aztecas'] = 0 },
+}
+
+--  Side 2 created by Rinehart
+Config.DoorList['Aztecas- Side 2'] = {
+    objCoords = vec3(1018.137024, -2528.662842, 28.415600),
+    locked = true,
+    objYaw = 84.999977111816,
+    doorLabel = 'Side 2',
+    distance = 2,
+    doorRate = 1.0,
+    fixText = false,
+    objName = -698797376,
+    doorType = 'door',
+    authorizedGangs = { ['aztecas'] = 0 },
+}
+
+-- Front created by Rinehart
+Config.DoorList['Aztecas-Front'] = {
+    doors = {
+        {objName = -1209169843, objYaw = 355.0, objCoords = vec3(1010.207458, -2531.838135, 28.451242)},
+        {objName = -1209169843, objYaw = 174.99998474121, objCoords = vec3(1007.620056, -2531.611816, 28.451242)}
+    },
+    distance = 2,
+    locked = true,
+    doorRate = 1.0,
+    doorLabel = 'Front',
+    doorType = 'double',
+    authorizedGangs = { ['aztecas'] = 0 },
+}
+
+-- Meeting room created by Rinehart
+Config.DoorList['Aztecas-Meeting room'] = {
+    doors = {
+        {objName = -1209169843, objYaw = 175.0, objCoords = vec3(1019.905701, -2539.223145, 32.443886)},
+        {objName = -1209169843, objYaw = 355.0, objCoords = vec3(1022.493286, -2539.449707, 32.443886)}
+    },
+    distance = 2,
+    locked = true,
+    doorRate = 1.0,
+    doorLabel = 'Meeting room',
+    doorType = 'double',
+    authorizedGangs = { ['aztecas'] = 0 },
+}
+
+-- office 1 created by Rinehart
+Config.DoorList['Aztecas-office 1'] = {
+    objCoords = vec3(1031.311401, -2543.171387, 32.455250),
+    locked = true,
+    objYaw = 84.999969482422,
+    doorLabel = 'office 1',
+    distance = 2,
+    doorRate = 1.0,
+    fixText = false,
+    objName = -1209169843,
+    doorType = 'door',
+    authorizedGangs = { ['aztecas'] = 0 },
+}
+
+-- office 2 created by Rinehart
+Config.DoorList['Aztecas-office 2'] = {
+    objCoords = vec3(1027.728882, -2546.238281, 32.450855),
+    locked = true,
+    objYaw = 174.99998474121,
+    doorLabel = 'office 2',
+    distance = 2,
+    doorRate = 1.0,
+    fixText = false,
+    objName = -1209169843,
+    doorType = 'door',
+    authorizedGangs = { ['aztecas'] = 0 },
+}
+
+-- Locker created by Rinehart
+Config.DoorList['Aztecas-Locker'] = {
+    objCoords = vec3(1023.172180, -2542.312012, 28.393375),
+    locked = true,
+    objYaw = 84.999977111816,
+    doorLabel = 'Locker',
+    distance = 2,
+    doorRate = 1.0,
+    fixText = false,
+    objName = -698797376,
+    doorType = 'door',
+    authorizedGangs = { ['aztecas'] = 0 },
+}
+
+-- Bay created by Rinehart
+Config.DoorList['Aztecas-Bay'] = {
+    objCoords = vec3(1032.426025, -2512.418457, 28.586565),
+    authorizedGangs = { ['aztecas'] = 0 },
+    locked = true,
+    objName = 1544229216,
+    doorType = 'garage',
+    distance = 3,
+    doorLabel = 'Bay',
+    fixText = false,
+    doorRate = 1.0,
+    objYaw = 265.00003051758,
+}
