58476a43aa931ddddc5c88a21bb868056b383465
Jay <80186604+jay-fivem@users.noreply.github.com>
2023-08-15 19:55:20 +0930

    azteca gang

--- resources/[framework]/qb-core/shared/gangs.lua
+++ resources/[framework]/qb-core/shared/gangs.lua
@@ -115,7 +115,25 @@ QBShared.Gangs = {
 				isboss = true
             },
         },
-	}
+	},
+    ['aztecas'] = {
+		label = 'Aztecas',
+		grades = {
+            ['0'] = {
+                name = 'Recruit'
+            },
+			['1'] = {
+                name = 'Enforcer'
+            },
+			['2'] = {
+                name = 'Shot Caller'
+            },
+			['3'] = {
+                name = 'Boss',
+				isboss = true
+            },
+        },
+	},
 }
 
 exports("GetSharedGangs", function()
