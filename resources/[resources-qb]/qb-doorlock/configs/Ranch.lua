63b35f385789f1b4b9b5ba589d19cd6a4a881af8
Ordinary <georgeebdonyt@gmail.com>
2023-08-15 12:18:38 +0100

    doorlokcs

---
+++ resources/[resources-qb]/qb-doorlock/configs/Ranch.lua
@@ -0,0 +1,185 @@
+
+
+-- Out 1 created by Rinehart
+Config.DoorList['Ranch-Out 1'] = {
+    doorLabel = 'Out 1',
+    distance = 2,
+    doors = {
+        {objName = -52575179, objYaw = 89.667892456055, objCoords = vec3(1390.424438, 1163.437744, 114.487251)},
+        {objName = -1032171637, objYaw = 89.667892456055, objCoords = vec3(1390.411499, 1161.240723, 114.487251)}
+    },
+    locked = true,
+    authorizedGangs = { ['cartel'] = 0 },
+    doorRate = 1.0,
+    doorType = 'double',
+}
+
+-- front created by Rinehart
+Config.DoorList['Ranch-front'] = {
+    doorLabel = 'front',
+    distance = 2,
+    doors = {
+        {objName = -228773386, objYaw = 270.00003051758, objCoords = vec3(1395.920288, 1142.904053, 114.790207)},
+        {objName = -228773386, objYaw = 89.999961853027, objCoords = vec3(1395.919922, 1140.704590, 114.790207)}
+    },
+    locked = true,
+    authorizedGangs = { ['cartel'] = 0 },
+    doorRate = 1.0,
+    doorType = 'double',
+}
+
+-- Out 2 created by Rinehart
+Config.DoorList['Ranch-Out 2'] = {
+    doorLabel = 'Out 2',
+    distance = 2,
+    doors = {
+        {objName = -52575179, objYaw = 89.999961853027, objCoords = vec3(1390.665771, 1133.317261, 114.480843)},
+        {objName = -1032171637, objYaw = 89.999961853027, objCoords = vec3(1390.665771, 1131.117065, 114.480843)}
+    },
+    locked = true,
+    authorizedGangs = { ['cartel'] = 0 },
+    doorRate = 1.0,
+    doorType = 'double',
+}
+
+-- Out 3 created by Rinehart
+Config.DoorList['Ranch-Out 3'] = {
+    doorLabel = 'Out 3',
+    distance = 2,
+    doors = {
+        {objName = -52575179, objYaw = 179.99998474121, objCoords = vec3(1399.392944, 1128.314209, 114.483597)},
+        {objName = -1032171637, objYaw = 179.99998474121, objCoords = vec3(1401.590454, 1128.314209, 114.483597)}
+    },
+    locked = true,
+    authorizedGangs = { ['cartel'] = 0 },
+    doorRate = 1.0,
+    doorType = 'double',
+}
+
+-- hidden out created by Rinehart
+Config.DoorList['Ranch-hidden out'] = {
+    doorLabel = 'Hidden out',
+    objCoords = vec3(1407.548218, 1128.328735, 114.485451),
+    objName = -228773386,
+    doorRate = 1.0,
+    objYaw = 180.06739807129,
+    doorType = 'door',
+    distance = 2,
+    authorizedGangs = { ['cartel'] = 0 },
+    locked = true,
+    fixText = false,
+}
+
+-- Back created by Rinehart
+Config.DoorList['Ranch-Back'] = {
+    authorizedGangs = { ['cartel'] = 0 },
+    doors = {
+        {objName = -1032171637, objYaw = 89.999961853027, objCoords = vec3(1409.292114, 1146.254150, 114.486916)},
+        {objName = -52575179, objYaw = 89.999961853027, objCoords = vec3(1409.292236, 1148.454346, 114.486916)}
+    },
+    doorLabel = 'Back',
+    locked = true,
+    doorType = 'double',
+    doorRate = 1.0,
+    distance = 2,
+}
+
+-- Out 4 created by Rinehart
+Config.DoorList['Ranch-Out 4'] = {
+    authorizedGangs = { ['cartel'] = 0 },
+    doors = {
+        {objName = -1032171637, objYaw = 89.667892456055, objCoords = vec3(1408.157471, 1158.955811, 114.487251)},
+        {objName = -52575179, objYaw = 90.11840057373, objCoords = vec3(1408.167480, 1161.154785, 114.487251)}
+    },
+    doorLabel = 'Out 4',
+    locked = true,
+    doorType = 'double',
+    doorRate = 1.0,
+    distance = 2,
+}
+
+-- Out 5 created by Rinehart
+Config.DoorList['Ranch-Out 5'] = {
+    authorizedGangs = { ['cartel'] = 0 },
+    doors = {
+        {objName = -1032171637, objYaw = 90.312919616699, objCoords = vec3(1408.170532, 1163.632812, 114.487251)},
+        {objName = -52575179, objYaw = 90.023124694824, objCoords = vec3(1408.165527, 1165.833740, 114.487251)}
+    },
+    doorLabel = 'Out 5',
+    locked = true,
+    doorType = 'double',
+    doorRate = 1.0,
+    distance = 2,
+}
+
+-- Boss Office created by Rinehart
+Config.DoorList['Ranch-Boss Office'] = {
+    authorizedGangs = { ['cartel'] = 0 },
+    locked = true,
+    doorType = 'door',
+    objYaw = 270.00003051758,
+    objName = 736699661,
+    fixText = false,
+    objCoords = vec3(1397.062378, 1164.739746, 114.483452),
+    distance = 2,
+    doorRate = 1.0,
+    doorLabel = 'Boss Office',
+}
+
+
+
+-- Balcony 2 created by Rinehart
+Config.DoorList['Ranch-Balcony 2'] = {
+    doorLabel = 'Balcony 2',
+    doors = {
+        {objName = -52575179, objYaw = 89.999977111816, objCoords = vec3(1393.987549, 1142.898560, 117.955887)},
+        {objName = -52575179, objYaw = 269.99996948242, objCoords = vec3(1393.987549, 1140.697510, 117.955887)}
+    },
+    distance = 2,
+    doorType = 'double',
+    authorizedGangs = { ['cartel'] = 0 },
+    doorRate = 1.0,
+    locked = true,
+}
+
+-- Balcony 1 created by Rinehart
+Config.DoorList['Ranch-Balcony 1'] = {
+    doorLabel = 'Balcony 1',
+    doors = {
+        {objName = -52575179, objYaw = 89.667892456055, objCoords = vec3(1394.084351, 1153.855103, 117.924530)},
+        {objName = -52575179, objYaw = 269.66790771484, objCoords = vec3(1394.071655, 1151.671265, 117.924530)}
+    },
+    distance = 2,
+    doorType = 'double',
+    authorizedGangs = { ['cartel'] = 0 },
+    doorRate = 1.0,
+    locked = true,
+}
+
+-- Boss bed created by Rinehart
+Config.DoorList['Ranch-Boss bed'] = {
+    doorLabel = 'Boss bed',
+    objCoords = vec3(1401.060303, 1158.948853, 117.641243),
+    objName = 736699661,
+    authorizedGangs = { ['cartel'] = 0 },
+    fixText = false,
+    objYaw = 89.999977111816,
+    doorType = 'door',
+    locked = true,
+    doorRate = 1.0,
+    distance = 2,
+}
+
+-- Garage created by Rinehart
+Config.DoorList['Ranch-Garage'] = {
+    doorRate = 1.0,
+    doors = {
+        {objName = -821436402, objYaw = 0.14956742525101, objCoords = vec3(1411.017944, 1113.987793, 113.694366)},
+        {objName = -821436402, objYaw = 179.93951416016, objCoords = vec3(1404.292969, 1113.963989, 113.695457)}
+    },
+    doorLabel = 'Garage',
+    authorizedGangs = { ['cartel'] = 0 },
+    locked = true,
+    distance = 8,
+    doorType = 'double',
+}
