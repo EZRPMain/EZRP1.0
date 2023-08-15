46eef4e1260e24225e01e0aa90e4ce9982da5446
Jay <80186604+jay-fivem@users.noreply.github.com>
2023-08-15 18:18:09 +0930

    Create humane.lua

---
+++ resources/[resources-qb]/qb-doorlock/configs/humane.lua
@@ -0,0 +1,59 @@
+Config.DoorList['humane'] = {
+	doors = {
+		{objHash = -1081024910, objHeading = 148.2, objCoords = vector3(3620.89, 3751.39, 28.69)},
+		{objHash = -1081024910, objHeading = 148.2, objCoords = vector3(3627.34, 3746.91, 28.69)}
+	},
+	slides = true,
+	lockpick = false,
+	locked = true,
+	garage = true,
+	maxDistance = 1.0,
+	fixText = false,
+	audioRemote = false,
+	authorizedJobs = { ['police']=0 },
+}
+
+Config.DoorList['humane2'] = {
+	doors = {
+		{objHash = 161378502, objHeading = 148.2, objCoords = vector3(3554.86, 3664.89, 28.12)},
+		{objHash = -1572101598, objHeading = 148.2, objCoords = vector3(3553.4, 3665.27, 28.12)}
+	},
+	slides = true,
+	lockpick = false,
+	locked = true,
+	garage = false,
+	maxDistance = 1.0,
+	fixText = false,
+	audioRemote = false,
+	authorizedJobs = { ['police']=0 },
+}
+
+Config.DoorList['humane3'] = {
+	doors = {
+		{objHash = -421709054, objHeading = 329.88, objCoords = vector3(3600.35, 3719.06, 29.69)},
+		{objHash = 1282049587, objHeading = 329.88, objCoords = vector3(3601.59, 3718.15, 29.69)}
+	},
+	slides = false,
+	lockpick = false,
+	locked = true,
+	garage = false,
+	maxDistance = 1.0,
+	fixText = false,
+	audioRemote = false,
+	authorizedJobs = { ['police']=0 },
+}
+
+Config.DoorList['humane4'] = {
+	doors = {
+		{objHash = -421709054, objHeading = 329.88, objCoords = vector3(3596.85, 3690.22, 28.82)},
+		{objHash = 1282049587, objHeading = 329.88, objCoords = vector3(3598.12, 3689.3, 28.82)}
+	},
+	slides = false,
+	lockpick = false,
+	locked = true,
+	garage = false,
+	maxDistance = 1.0,
+	fixText = false,
+	audioRemote = false,
+	authorizedJobs = { ['police']=0 },
+}
