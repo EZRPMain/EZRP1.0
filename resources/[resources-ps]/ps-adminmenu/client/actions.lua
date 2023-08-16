df325be598309aa3116e47a8967c56b96c277648
Jay <80186604+jay-fivem@users.noreply.github.com>
2023-08-16 18:21:53 +0930

    Update actions.lua

--- resources/[resources-ps]/ps-adminmenu/client/actions.lua
+++ resources/[resources-ps]/ps-adminmenu/client/actions.lua
@@ -1186,6 +1186,38 @@ end)
 
 
 -- Set Model
+local blockedPeds = {
+    "mp_m_freemode_01",
+    "mp_f_freemode_01",
+    "tony",
+    "g_m_m_chigoon_02_m",
+    "u_m_m_jesus_01",
+    "a_m_y_stbla_m",
+    "ig_terry_m",
+    "a_m_m_ktown_m",
+    "a_m_y_skater_m",
+    "u_m_y_coop",
+    "ig_car3guy1_m",
+}
+
+
+local function LoadPlayerModel(skin)
+    RequestModel(skin)
+    while not HasModelLoaded(skin) do
+      Wait(0)
+    end
+end
+
+local function isPedAllowedRandom(skin)
+    local retval = false
+    for _, v in pairs(blockedPeds) do
+        if v ~= skin then
+            retval = true
+        end
+    end
+    return retval
+end
+
 RegisterNetEvent('ps-adminmenu:client:SetModel', function(inputData)
     local ped = PlayerPedId()
     local model = GetHashKey(inputData["Model"])
