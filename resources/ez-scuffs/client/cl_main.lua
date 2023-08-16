0da778a8f752304dd84930f5ef9add46bc8eddd9
Jay <80186604+jay-fivem@users.noreply.github.com>
2023-08-16 14:35:00 +0930

    Update cl_main.lua

--- resources/ez-scuffs/client/cl_main.lua
+++ resources/ez-scuffs/client/cl_main.lua
@@ -1,18 +1,9 @@
 RegisterNetEvent("scuff-fix", function()
     local ped = PlayerPedId()
     local coords = GetEntityCoords(ped)
-    if IsEntityDead(ped) then
+    if IsEntityDead(ped) or IsPedRagdoll(ped) then
         print("Dead Scuff Fix")
-        CreateThread(function()
-            local timeout = 0
-            repeat
-                Wait(0)
-                timeout = timeout + 1
-                SetEntityCoords(ped, coords)
-            until timeout >= 1000
-            timeout = 0
-        end)
-        -- SetEntityCoords(ped, coords)
+        SetEntityCoords(ped, coords)
     end
 end)
 
