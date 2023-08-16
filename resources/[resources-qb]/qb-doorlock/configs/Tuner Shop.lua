2024084e69bc6faadb91046cc9a6cac041a48ff0
Ordinary <georgeebdonyt@gmail.com>
2023-08-16 05:45:53 +0100

    Create Tuner Shop.lua

---
+++ resources/[resources-qb]/qb-doorlock/configs/Tuner Shop.lua
@@ -0,0 +1,57 @@
+
+
+-- Bay 1 created by Rinehart
+Config.DoorList['Tuner Shop-Bay 1'] = {
+    doorLabel = 'Bay 1',
+    doorType = 'garage',
+    objCoords = vec3(154.809525, -3023.889160, 8.503336),
+    doorRate = 1.0,
+    objName = -456733639,
+    authorizedJobs = { ['tunershop'] = 0 },
+    fixText = false,
+    locked = false,
+    distance = 5,
+    objYaw = 89.999961853027,
+}
+
+-- Bay 2 created by Rinehart
+Config.DoorList['Tuner Shop-Bay 2'] = {
+    doorLabel = 'Bay 2',
+    doorType = 'garage',
+    objCoords = vec3(154.809525, -3034.051270, 8.503336),
+    doorRate = 1.0,
+    objName = -456733639,
+    authorizedJobs = { ['tunershop'] = 0 },
+    fixText = false,
+    locked = false,
+    distance = 5,
+    objYaw = 89.999961853027,
+}
+
+-- side created by Rinehart
+Config.DoorList['Tuner Shop-side'] = {
+    doorLabel = 'side',
+    doorType = 'door',
+    objCoords = vec3(154.934464, -3017.322998, 7.190679),
+    doorRate = 1.0,
+    objName = -2023754432,
+    authorizedJobs = { ['tunershop'] = 0 },
+    fixText = false,
+    locked = false,
+    distance = 2,
+    objYaw = 270.00003051758,
+}
+
+-- Boss created by Rinehart
+Config.DoorList['Tuner Shop-Boss'] = {
+    doorLabel = 'Boss',
+    doorType = 'door',
+    objCoords = vec3(151.466797, -3011.705078, 7.258367),
+    doorRate = 1.0,
+    objName = -1229046235,
+    authorizedJobs = { ['tunershop'] = 0 },
+    fixText = false,
+    locked = false,
+    distance = 2,
+    objYaw = 89.999977111816,
+}
