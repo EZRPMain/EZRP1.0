5d41c3607a5e2c28337a42ed46176802d106d329
Ordinary <georgeebdonyt@gmail.com>
2023-08-16 01:08:29 +0100

    Create lostmc.lua

---
+++ resources/[resources-qb]/qb-doorlock/configs/lostmc.lua
@@ -0,0 +1,15 @@
+
+
+-- MC front 1 created by Rinehart
+Config.DoorList['lostmc-MC front 1'] = {
+    authorizedGangs = { ['lostmc'] = 0 },
+    objYaw = 42.65185546875,
+    objName = 190770132,
+    doorRate = 1.0,
+    fixText = false,
+    locked = false,
+    doorType = 'door',
+    distance = 2,
+    doorLabel = 'MC front 1',
+    objCoords = vec3(981.150513, -103.255249, 74.993576),
+}
