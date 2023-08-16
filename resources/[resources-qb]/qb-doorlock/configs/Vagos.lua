9a32f141719fb3a3324e450165dfa5563e6d4dab
Ordinary <georgeebdonyt@gmail.com>
2023-08-16 05:59:50 +0100

    doorlocks

---
+++ resources/[resources-qb]/qb-doorlock/configs/Vagos.lua
@@ -0,0 +1,15 @@
+
+
+-- Front created by Rinehart
+Config.DoorList['Vagos-Front'] = {
+    doorLabel = 'front',
+    doorType = 'door',
+    authorizedGangs = { ['vagos'] = 0 },
+    doorRate = 1.0,
+    objName = 464151082,
+    distance = 2,
+    fixText = false,
+    locked = false,
+    objCoords = vec3(134.395447, -2204.097412, 7.514325),
+    objYaw = 270.0,
+}
