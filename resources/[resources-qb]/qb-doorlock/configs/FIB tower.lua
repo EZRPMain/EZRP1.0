39a60d85e752e018b336c6342c2c3e8c4c7d640a
Ordinary <georgeebdonyt@gmail.com>
2023-08-16 04:51:21 +0100

    Update FIB tower.lua

--- resources/[resources-qb]/qb-doorlock/configs/FIB tower.lua
+++ resources/[resources-qb]/qb-doorlock/configs/FIB tower.lua
@@ -13,3 +13,17 @@ Config.DoorList['FIB tower-Door one top'] = {
     doorLabel = 'Door one top',
     doorRate = 1.0,
 }
+
+-- front door 1 created by Rinehart
+Config.DoorList['FIB tower-front door 1'] = {
+    authorizedJobs = { ['police'] = 0 },
+    distance = 2,
+    locked = false,
+    doorType = 'double',
+    doors = {
+        {objName = -90456267, objYaw = 84.050018310547, objCoords = vec3(105.760674, -746.645996, 46.182655)},
+        {objName = -1517873911, objYaw = 78.216918945312, objCoords = vec3(106.379272, -742.698242, 46.181709)}
+    },
+    doorLabel = 'front door 1',
+    doorRate = 1.0,
+}
