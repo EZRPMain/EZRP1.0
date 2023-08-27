766842dc3497cfd3da3fba4dd6bd4a75aaec952e
Jay <80186604+jay-fivem@users.noreply.github.com>
2023-08-27 19:56:23 +0930

    Update server.lua

--- resources/[resources-custom]/jay-windowtint/server/server.lua
+++ resources/[resources-custom]/jay-windowtint/server/server.lua
@@ -1,7 +1,7 @@
 -- Create Useable Item QBCore
-QBCore.Functions.CreateUseableItem("tintreader", function(source, item)
+exports['qb-core']:CreateUseableItem("tintreader", function(source, item)
     local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
+    local Player = exports['qb-core']:GetPlayer(src)
     if Player.Functions.GetItemByName(item.name) then
         TriggerClientEvent("cl_checkTint", src)
     end
