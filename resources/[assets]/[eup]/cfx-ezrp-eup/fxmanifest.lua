18579f11a4b5803db5dc0043054a51bed2bd04ef
Jay <80186604+jay-fivem@users.noreply.github.com>
2023-08-28 23:57:14 +0930

    hopeful;ly work god pls

--- resources/[assets]/[eup]/cfx-ezrp-eup/fxmanifest.lua
+++ resources/[assets]/[eup]/cfx-ezrp-eup/fxmanifest.lua
@@ -4,7 +4,9 @@ fx_version 'bodacious'
 game { 'gta5' }
 
 files {
-  'mp_m_freemode_01_mp_m_ezrp.meta'
+  'mp_m_freemode_01_mp_m_ezrp.meta',
+  'mp_f_freemode_01_mp_f_ezrp.meta'
 }
 
 data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_ezrp.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_ezrp.meta'
