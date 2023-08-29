1bf061adc0594fed507b93d3d2b9f9345a03be29
Jay <80186604+jay-fivem@users.noreply.github.com>
2023-08-29 22:48:16 +0930

    Create comicdoor.lua

---
+++ resources/[resources-qb]/qb-doorlock/configs/comicdoor.lua
@@ -0,0 +1,15 @@
+
+
+-- comicdoor created by Jay
+Config.DoorList['comicdoor-comicdoor'] = {
+    doorType = 'doublesliding',
+    doorLabel = 'comicdoor',
+    doors = {
+        {objName = 895322462, objYaw = 89.915664672852, objCoords = vec3(392.241577, -751.150208, 28.682714)},
+        {objName = 895322462, objYaw = 270.18600463867, objCoords = vec3(392.292603, -753.524780, 28.682714)}
+    },
+    authorizedJobs = { ['police'] = 0 },
+    distance = 3,
+    doorRate = 1.0,
+    locked = false,
+}
