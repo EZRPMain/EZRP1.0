0d54672aa6dad8f8434b3e60fc5fe19ede8cc1fd
Ordinary <georgeebdonyt@gmail.com>
2023-09-14 21:04:55 +0100

    stuffs yk

--- resources/EZ-ConnectQueue/config.lua
+++ resources/EZ-ConnectQueue/config.lua
@@ -1,5 +1,5 @@
 Config = {
-	Default_Prio = 500, -- This is the default priority value if a discord isn't found
+	Default_Prio = 550, -- This is the default priority value if a discord isn't found
 	AllowedPerTick = 1, -- How many players should we allow to connect at a time?
 	CheckForGhostUsers = 40, -- How many seconds should the script check for ghosts users in the queue?
 	HostDisplayQueue = true,
@@ -8,24 +8,24 @@ Config = {
 		Discord = true,
 		Steam = false
 	},
-	WhitelistRequired = false, -- If this option is set to true, a player must have a role in Config.Rankings to be allowed into the server
+	WhitelistRequired = true, -- If this option is set to true, a player must have a role in Config.Rankings to be allowed into the server
 	Debug = false,
 	Webhook = 'https://discord.com/api/webhooks/1147975278296051773/gH-0QZ4TtHM3OSve7ZC_hX_9UTuO8XRMccNkA_jMs742WT2zTyxpUWqYgwhpPzgvKw-2',
 	Displays = {
 		Prefix = 'Eazy RP Connect Queue',
 		ConnectingLoop = { 
-			'💙🌐🔵',
-			'🔵💙🌐',
-			'🌐🔵💙',
-			'🔵💙🌐',
+			'💙🌐🔵E',
+			'Z🔵💙🌐',
+			'🌐R🔵💙',
+			'🔵💙P🌐',
 			'💙🌐🔵',
 		},
 		Messages = {
 			MSG_CONNECTING = 'You are being connected [{QUEUE_NUM}/{QUEUE_MAX}]: ', -- Default message if they have no discord roles 
-			MSG_CONNECTED = 'You are up! You are being connected now :)',
-			MSG_DISCORD_REQUIRED = 'Your Discord was not detected... You are required to have Discord to play on this server... Discord.gg/ePxWrZ9JsP',
+			MSG_CONNECTED = 'The Train Arrived! If you disconnect you will need to wait in queue:)',
+			MSG_DISCORD_REQUIRED = 'Your Discord was not detected... You are required to have Discord/Join our Discord to play on Eazy RP... Discord.gg/ePxWrZ9JsP',
 			MSG_STEAM_REQUIRED = 'Your Steam was not detected... Open Steam and Restart FiveM.',
-			MSG_NOT_WHITELISTED = 'You do not have a Discord role whitelisted for this server... You are not whitelisted.',
+			MSG_NOT_WHITELISTED = 'This Shouldnt happen! Please Screenshot to the discord #bug-reports.',
 		},
 	},
 }
