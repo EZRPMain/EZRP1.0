70d13ccb86287d9f2f5ad32352cdb4c18464e1c1
Jay <itexlzvisual@gmail.com>
2024-03-24 23:41:51 +1030

    bunker

---
+++ resources/[assets]/[mlos]/eazy_zancudo_addon/fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version 'cerulean'
+game 'gta5'
+files {
+    'audio/*.dat10',
+    'audio/*.dat10.nametable',
+    'audio/*.dat10.rel',
+    'audio/*.dat151',
+    'audio/*.dat151.nametable',
+    'audio/*.dat151.rel',
+    'audio/*.dat54',
+    'audio/*.dat54.nametable',
+    'audio/*.dat54.rel',
+}
+
+data_file 'AUDIO_GAMEDATA' 'audio/a69bunker_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/a69bunker_sounds.dat'
+
+data_file 'AUDIO_GAMEDATA' 'audio/a69carrier_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/a69carrier_sounds.dat'
+
+data_file 'AUDIO_GAMEDATA' 'audio/a69sewers_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/a69sewers_sounds.dat'
+
+data_file 'AUDIO_GAMEDATA' 'audio/a69storage_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/a69storage_sounds.dat'
+
+data_file 'AUDIO_GAMEDATA' 'audio/a69vaultroom_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/a69vaultroom_sounds.dat'
+
+this_is_a_map 'yes'
---
+++ resources/[resources-qb]/qb-doorlock/configs/secret-bunker.lua
@@ -0,0 +1,29 @@
+
+
+-- secret-bunker-secret created by Jay
+Config.DoorList['secret-bunker-secret-bunker-secret'] = {
+    objYaw = 4.9540519714355,
+    fixText = false,
+    distance = 5,
+    doorType = 'door',
+    objName = 11733050,
+    authorizedJobs = { ['police'] = 0 },
+    doorRate = 1.0,
+    objCoords = vec3(-1828.896484, 4524.974121, 5.429381),
+    locked = true,
+    doorLabel = 'secret-bunker-secret',
+}
+
+-- secret-bunker-enter created by Jay
+Config.DoorList['secret-bunker-secret-bunker-enter'] = {
+    locked = true,
+    doors = {
+        {objName = 1252997920, objYaw = 19.997058868408, objCoords = vec3(-1840.639771, 4544.431641, 5.221344)},
+        {objName = 1252997920, objYaw = 200.00299072266, objCoords = vec3(-1846.135254, 4542.431152, 5.221344)}
+    },
+    doorType = 'doublesliding',
+    distance = 6,
+    authorizedJobs = { ['police'] = 0 },
+    doorLabel = 'secret-bunker-enter',
+    doorRate = 1.0,
+}
