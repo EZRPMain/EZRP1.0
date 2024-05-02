f655543af8365d94a0cd1754b7b054dd050a026e
Hxpnotic <76715282+Botsitosss@users.noreply.github.com>
2024-05-02 02:13:01 -0500

    Spawn Menu Fixed
    Blips Fixed just missing apartment

--- resources/[resources-qb]/qb-spawn/config.lua
+++ resources/[resources-qb]/qb-spawn/config.lua
@@ -5,14 +5,14 @@ QB.Spawns = {
         coords = vector4(195.17, -933.77, 29.7, 144.5),
         location = "legion",
         label = "Legion Square",
-        pos = {top = 50.9, left = 65.5}
+        pos = {top = 50, left = 61.9}
     },
 
     ["policedp"] = {
         coords = vector4(428.23, -984.28, 29.76, 3.5),
         location = "policedp",
         label = "Police Department",
-        pos = {top = 48.4, left = 65.8}
+        pos = {top = 48, left = 57.5}
     },
 
     ["paleto"] = {
@@ -26,28 +26,28 @@ QB.Spawns = {
         coords = vector4(327.56, -205.08, 53.08, 163.5),
         location = "motel",
         label = "Motels",
-        pos = {top = 49.5, left = 62}
+        pos = {top = 49.5, left = 52}
     },
 
     ["sandy"] = {
-        coords = vector4(1151.608154296875, 2667.66650390625, 38.14596557617187, 356.8353271484375),
+        coords = vector4(1857.37, 3680.04, 33.79, 213.5),
         location = "sandy",
-        label = "Sandy Shores",
-        pos = {top = 41.5, left = 47.6},
+        label = "Blaine County Sheriff Station",
+        pos = {top = 33, left = 28.7},
     },
 
     ["amusementpark"] = {
         coords = vector4(-1600.0509033203125, -971.2437133789064, 13.01739120483398, 54.69508361816406),
         location = "amusementpark",
         label = "Amusement Park",
-        pos = {top = 67, left = 65.7},
+        pos = {top = 72, left = 57.8},
     },
 
     ["paleto2"] = {
         coords = vector4(-375.2769775390625, 6030.4697265625, 31.52615165710449, 162.48745727539065),
         location = "paleto2",
         label = "Paleto Bay Sheriff",
-        pos = {top = 55.8, left = 30.9},
+        pos = {top = 58, left = 14},
     },
 }
 
